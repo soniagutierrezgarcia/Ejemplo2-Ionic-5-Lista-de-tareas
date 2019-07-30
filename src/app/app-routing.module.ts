cadfdac350d51604c25cc8cebe08c4b6a7a6087b
Sonia <sonia_gutierrez_garcia@hotmail.com>
2019-07-30 18:11:56 +0200

    'Cambios'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: 'todos', loadChildren: './todos/todos.module#TodosPageModule' },
+  { path: 'add-task-modal', loadChildren: './add-task-modal/add-task-modal.module#AddTaskModalPageModule' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AddTaskModalPage } from './add-task-modal/add-task-modal.page';
+import { FormsModule } from '@angular/forms';
+import { TodoServiceService } from './services/todo-service.service';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, AddTaskModalPage],
+  entryComponents: [AddTaskModalPage],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    TodoServiceService
   ],
   bootstrap: [AppComponent]
 })
